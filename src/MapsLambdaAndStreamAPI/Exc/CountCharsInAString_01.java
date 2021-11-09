e343c8c4f6ac7fe4ea7095ac4c0e0d4f6c837a7a
ivanovasilvana <sisi_965@abv.bg>
2021-11-09 20:18:07 +0200

    MapsLambdaAndStreamAPI/Exc/CountCharsInAString_01

---
+++ src/MapsLambdaAndStreamAPI/Exc/CountCharsInAString_01.java
@@ -0,0 +1,31 @@
+package MapsLambdaAndStreamAPI.Exc;
+
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class CountCharsInAString_01 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String text = scanner.nextLine();
+        Map<Character, Integer> countChars = new LinkedHashMap<>();
+        for (char symbol : text.toCharArray()) {
+            if (symbol == ' ') {
+                continue;
+            }
+            if (!countChars.containsKey(symbol)) {
+                countChars.put(symbol, 1);
+            } else {
+                int currentCount = countChars.get(symbol);
+                countChars.put(symbol, currentCount + 1);
+            }
+        }
+
+        /* countChars.entrySet().forEach(entry -> System.out.println(entry.getKey() + " -> " + entry.getValue()));*/
+        for (Map.Entry<Character, Integer> entry : countChars.entrySet()) {
+            System.out.println(entry.getKey() + " -> " + entry.getValue());
+        }
+    }
+}
+
