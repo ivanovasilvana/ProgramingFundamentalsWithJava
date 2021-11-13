7f14228c4e18f1252502413a5e0661adc279171d
ivanovasilvana <sisi_965@abv.bg>
2021-11-13 18:32:59 +0200

    update

--- src/MapsLambdaAndStreamAPI/Exc/LegendaryFarming_03.java
+++ src/MapsLambdaAndStreamAPI/Exc/LegendaryFarming_03.java
@@ -1,6 +1,9 @@
 package MapsLambdaAndStreamAPI.Exc;
 
-import java.util.*;
+
+import java.util.Map;
+import java.util.Scanner;
+import java.util.TreeMap;
 
 public class LegendaryFarming_03 {
     public static void main(String[] args) {
@@ -15,26 +18,30 @@ public class LegendaryFarming_03 {
         while (!isOver) {
             String input = scanner.nextLine();
             String[] inputData = input.split(" ");
+
             for (int i = 0; i < inputData.length; i += 2) {
                 int quantity = Integer.parseInt(inputData[i]);
-                String material = inputData[i + 1].toLowerCase(Locale.ROOT);
+                String material = inputData[i + 1].toLowerCase();
 
                 if (material.equals("shards") || material.equals("fragments") || material.equals("motes")) {
                     int currentQuantity = materials.get(material);
                     materials.put(material, currentQuantity + quantity);
+
                     if (materials.get(material) >= 255) {
+
                         if (material.equals("shards")) {
                             System.out.println("Shadowmourne obtained!");
                         } else if (material.equals("fragments")) {
                             System.out.println("Valanyr obtained!");
                         } else {
-                            System.out.println("Dragonwrath  obtained!");
+                            System.out.println("Dragonwrath obtained!");
                         }
                         materials.put(material, materials.get(material) - 250);
                         isOver = true;
                         break;
                     }
                 } else {
+
                     if (!junks.containsKey(material)) {
                         junks.put(material, quantity);
                     } else {
