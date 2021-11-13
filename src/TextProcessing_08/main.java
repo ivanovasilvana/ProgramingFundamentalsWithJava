138954cc22f6a560eaf114ebf54814f85ab2dddd
ivanovasilvana <sisi_965@abv.bg>
2021-11-13 22:38:11 +0200

    update

--- src/TextProcessing_08/main.java
+++
@@ -1,9 +0,0 @@
-package TextProcessing_08;
-
-import java.util.Scanner;
-
-public class main {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-    }
-}
