1833c3105f59d62d47b9386fe96070ca9d40a023
ivanovasilvana <sisi_965@abv.bg>
2021-11-13 20:49:33 +0200

    MapsLambdaAndStreamAPI/Exc/StudentAcademy_07

---
+++ src/MapsLambdaAndStreamAPI/Exc/StudentAcademy_07.java
@@ -0,0 +1,41 @@
+package MapsLambdaAndStreamAPI.Exc;
+
+import java.util.*;
+
+public class StudentAcademy_07 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int lines = Integer.parseInt(scanner.nextLine());
+        Map<String, List<Double>> studentGrade = new HashMap<>();
+        for (int i = 0; i < lines; i++) {
+            String name = scanner.nextLine();
+            Double grade = Double.parseDouble(scanner.nextLine());
+
+            if (!studentGrade.containsKey(name)) {
+                studentGrade.put(name, new ArrayList<>());
+            }
+            studentGrade.get(name).add(grade);
+        }
+
+        Map<String, Double> studentAverageGrade = new LinkedHashMap<>();
+
+        for (Map.Entry<String, List<Double>> entry : studentGrade.entrySet()) {
+            String studentName = entry.getKey();
+            List<Double> grades = entry.getValue();
+            double averageGrade = getAverageGrade(grades);
+            if (averageGrade >= 4.50) {
+                studentAverageGrade.put(studentName, averageGrade);
+            }
+        }
+        studentAverageGrade.entrySet().stream().sorted((entry1, entry2) -> entry2.getValue().compareTo(entry1.getValue()))
+                .forEach(entry -> System.out.printf("%s -> %.2f%n", entry.getKey(), entry.getValue()));
+    }
+
+    public static double getAverageGrade(List<Double> grades) {
+        double sumGrades = 0;
+        for (double grade : grades) {
+            sumGrades += grade;
+        }
+        return sumGrades / grades.size();
+    }
+}
