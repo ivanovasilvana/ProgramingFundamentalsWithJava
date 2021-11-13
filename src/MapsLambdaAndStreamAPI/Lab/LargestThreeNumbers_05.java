e1ce4e109c74577cca71309580bcccca01bbb7be
ivanovasilvana <sisi_965@abv.bg>
2021-11-13 18:04:05 +0200

    MapsLambdaAndStreamAPI/Lab/LargestThreeNumbers_05

---
+++ src/MapsLambdaAndStreamAPI/Lab/LargestThreeNumbers_05.java
@@ -0,0 +1,22 @@
+package MapsLambdaAndStreamAPI.Lab;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+public class LargestThreeNumbers_05 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        List<Integer> numbers = Arrays
+                .stream(scanner.nextLine().split("\\s+"))
+                .map(Integer::parseInt)
+                .sorted((e1, e2) -> e2.compareTo(e1))
+                .limit(3)
+                .collect(Collectors.toList());
+        for (int num : numbers) {
+            System.out.print(num + " ");
+        }
+    }
+}
