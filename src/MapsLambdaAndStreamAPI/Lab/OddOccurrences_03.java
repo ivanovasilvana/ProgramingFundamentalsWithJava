1b53c6c2ebd724ca663335b45598b0021d8b49ef
ivanovasilvana <sisi_965@abv.bg>
2021-11-13 17:43:14 +0200

    MapsLambdaAndStreamAPI/Lab/OddOccurrences_03

---
+++ src/MapsLambdaAndStreamAPI/Lab/OddOccurrences_03.java
@@ -0,0 +1,34 @@
+package MapsLambdaAndStreamAPI.Lab;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class OddOccurrences_03 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        List<String> words = Arrays.stream(scanner.nextLine().toLowerCase().split("\\s+"))
+                .collect(Collectors.toList());
+        Map<String, Integer> countOfWord = new LinkedHashMap<>();
+        List<String> odd = new ArrayList<>();
+
+        for (int i = 0; i < words.size(); i++) {
+            String currentWord = words.get(i);
+            if (countOfWord.containsKey(currentWord)) {
+                countOfWord.put(currentWord, countOfWord.get(currentWord) + 1);
+            } else {
+                countOfWord.put(currentWord, 1);
+            }
+        }
+        for (Map.Entry<String, Integer> entry : countOfWord.entrySet()) {
+            if (entry.getValue() % 2 != 0) {
+                odd.add(entry.getKey());
+            }
+        }
+        for (int i = 0; i < odd.size(); i++) {
+            System.out.print(odd.get(i));
+            if (i < odd.size() - 1) {
+                System.out.print(", ");
+            }
+        }
+    }
+}
