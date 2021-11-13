72084a4e382b06cf8afe0a80f640b873c6400c09
ivanovasilvana <sisi_965@abv.bg>
2021-11-13 18:46:31 +0200

    MapsLambdaAndStreamAPI/Exc/LegendaryFarming_03

--- src/MapsLambdaAndStreamAPI/Exc/LegendaryFarming_03.java
+++ src/MapsLambdaAndStreamAPI/Exc/LegendaryFarming_03.java
@@ -8,53 +8,53 @@ import java.util.TreeMap;
 public class LegendaryFarming_03 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        Map<String, Integer> materials = new TreeMap<>();
+        Map<String, Integer> materials = new TreeMap<>();   //1. fragments   2. motes  3. shards`
         materials.put("shards", 0);
         materials.put("fragments", 0);
         materials.put("motes", 0);
-
+        //junk -> others
         Map<String, Integer> junks = new TreeMap<>();
         boolean isOver = false;
-        while (!isOver) {
+        while (!isOver) { //прекъсваме, когато купим item (isOver == true)
             String input = scanner.nextLine();
             String[] inputData = input.split(" ");
-
-            for (int i = 0; i < inputData.length; i += 2) {
-                int quantity = Integer.parseInt(inputData[i]);
-                String material = inputData[i + 1].toLowerCase();
-
+            for (int index = 0; index < inputData.length; index += 2) { //Обходим всеки ред
+                int quantity = Integer.parseInt(inputData[index]);
+                String material = inputData[index + 1].toLowerCase();
+                //проверка дали материалът е валиден -> shards, fragments, motes
                 if (material.equals("shards") || material.equals("fragments") || material.equals("motes")) {
                     int currentQuantity = materials.get(material);
                     materials.put(material, currentQuantity + quantity);
-
-                    if (materials.get(material) >= 255) {
-
+                    //проверак дали >= 250
+                    if (materials.get(material) >= 250) {
+                        //печелим item
                         if (material.equals("shards")) {
                             System.out.println("Shadowmourne obtained!");
                         } else if (material.equals("fragments")) {
                             System.out.println("Valanyr obtained!");
-                        } else {
+                        } else { //motes
                             System.out.println("Dragonwrath obtained!");
                         }
+                        //намалям количеството на съответния материал с 250
                         materials.put(material, materials.get(material) - 250);
                         isOver = true;
                         break;
                     }
                 } else {
-
                     if (!junks.containsKey(material)) {
                         junks.put(material, quantity);
                     } else {
-                        int currentQuantity = materials.get(material);
+                        int currentQuantity = junks.get(material);
                         junks.put(material, currentQuantity + quantity);
                     }
                 }
-
             }
         }
-        //сортиране на материалите по намаляващ ред на количеството
+
+        //сортираме материалите по намаляващ ред (descending order) на количестовото
         materials.entrySet().stream().sorted((entry1, entry2) -> entry2.getValue().compareTo(entry1.getValue()))
-                .forEach(entry -> System.out.println(entry.getKey() + ": " + entry.getValue()));
+                .forEach(entry -> System.out.printf("%s: %d%n", entry.getKey(), entry.getValue()));
+
         junks.entrySet().forEach(entry -> System.out.println(entry.getKey() + ": " + entry.getValue()));
     }
 }
