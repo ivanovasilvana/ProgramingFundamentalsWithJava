7db99b2bab8278bc24f79511afb51d3fa52e5f37
ivanovasilvana <sisi_965@abv.bg>
2021-11-13 21:08:09 +0200

    MapsLambdaAndStreamAPI/Exc/CompanyUsers_08

---
+++ src/MapsLambdaAndStreamAPI/Exc/CompanyUsers_08.java
@@ -0,0 +1,31 @@
+package MapsLambdaAndStreamAPI.Exc;
+
+import java.util.*;
+
+public class CompanyUsers_08 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Map<String, List<String>> companyUsers = new TreeMap<>();
+        String input = scanner.nextLine();
+
+        while (!input.equals("End")) {
+            String companyName = input.split(" -> ")[0];
+            String employeeId = input.split(" -> ")[1];
+
+            if (!companyUsers.containsKey(companyName)) {
+                companyUsers.put(companyName, new ArrayList<>());
+            }
+            if (!companyUsers.get(companyName).contains(employeeId)) {
+                companyUsers.get(companyName).add(employeeId);
+            }
+            input = scanner.nextLine();
+        }
+        companyUsers.entrySet().forEach(e -> {
+            System.out.println(e.getKey());
+            e.getValue()
+                    .stream()
+                    .forEach(user -> System.out.println("-- " + user));
+        });
+    }
+}
+
