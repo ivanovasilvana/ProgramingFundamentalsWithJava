ce9bf711eec1a9da9f6d10baeb62ba4dd6aa8584
ivanovasilvana <sisi_965@abv.bg>
2021-11-13 17:07:52 +0200

    MapsLambdaAndStreamAPI/Lab/WordSynonyms_02

---
+++ src/MapsLambdaAndStreamAPI/Lab/WordSynonyms_02.java
@@ -0,0 +1,24 @@
+package MapsLambdaAndStreamAPI.Lab;
+
+import java.util.*;
+
+public class WordSynonyms_02 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int count = Integer.parseInt(scanner.nextLine());
+
+        Map<String, List<String>> synonyms = new LinkedHashMap<>();
+
+        for (int i = 0; i < count; i++) {
+            String word = scanner.nextLine();
+            String synonym = scanner.nextLine();
+
+            synonyms.putIfAbsent(word, new ArrayList<>());
+            synonyms.get(word).add(synonym);
+        }
+        for (Map.Entry<String, List<String>> entry : synonyms.entrySet()) {
+            System.out.printf("%s - %s%n", entry.getKey(), String.join(", ", entry.getValue()));
+        }
+    }
+}
