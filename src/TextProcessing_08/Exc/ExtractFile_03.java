22e899e64ea82054984d3d75ff174b262897d250
ivanovasilvana <sisi_965@abv.bg>
2021-11-14 19:36:18 +0200

    TextProcessing_08/Exc/ExtractFile_03

---
+++ src/TextProcessing_08/Exc/ExtractFile_03.java
@@ -0,0 +1,17 @@
+package TextProcessing_08.Exc;
+
+import java.util.Scanner;
+
+public class ExtractFile_03 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String fullPathFile = scanner.nextLine();
+        String[] pathParts = fullPathFile.split("\\\\");
+        String fullFileName = pathParts[pathParts.length - 1];
+        String fileName = fullFileName.split("\\.")[0];
+        String extension = fullFileName.split("\\.")[1];
+
+        System.out.println("File name: " + fileName);
+        System.out.println("File extension: " + extension);
+    }
+}
