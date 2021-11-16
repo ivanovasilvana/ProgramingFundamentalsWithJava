69b804725056dfd25e95f2f08b1ca0d390881967
ivanovasilvana <sisi_965@abv.bg>
2021-11-16 18:28:57 +0200

    Arrays_03/MoreExc/PascalTriangle_02

--- src/Arrays_03/MoreExc/PascalTriangle_02.java
+++ src/Arrays_03/MoreExc/PascalTriangle_02.java
@@ -6,31 +6,25 @@ import java.util.Scanner;
 public class PascalTriangle_02 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int row = Integer.parseInt(scanner.nextLine());
-
-        int[] firstArray = {1};
-        int[] secondArray = {1, 1};
+        int numbers[] = new int[30];
 
 
-        for (int i = 0; i < secondArray.length + 1; i++) {
-            if (i == 0) {
-                System.out.println(Arrays.toString(firstArray).replaceAll("[\\[\\],]", ""));
-            } else if (i == 1) {
-                System.out.println(Arrays.toString(secondArray).replaceAll("[\\[\\],]", ""));
-            } else {
-                int[] array = new int[secondArray.length + 1];
-                array[0] = 1;
-                array[array.length - 1] = 1;
-                for (int j = 1; j < array.length - 1; j++) {
-                    array[j] = secondArray[j - 1] + secondArray[j];
-                }
-                System.out.println(Arrays.toString(array).replaceAll("[\\[\\]\\,]", ""));
-                secondArray = array;
+        int row = scanner.nextInt();
 
+
+        for (int i = 0; i < row; i++)
+        {
+
+            numbers[i] = 1;
+            for (int j = 0; j <= i; j++)
+            {
+                System.out.print(numbers[i] + " ");
+                numbers[i] = numbers[i] * (i - j) / (j + 1);
+            }
+            System.out.println();
             }
         }
     }
-
-}
+    
 
 
